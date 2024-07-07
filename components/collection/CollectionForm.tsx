4a4ac69428d117474a585977da7b5cc8d18fa385
Praveen <praveensingh55602@gmail.com>
2024-07-07 23:38:56 +0530

    nav bar created

---
+++ components/collection/CollectionForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { z } from "zod";
+import React from "react";
+import { Separator } from "../ui/separator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string(),
+});
+
+export const CollectionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
+  return (
+    <div className="p-10">
+      <p className="text-heading2-bold">Create Collection</p>
+      <Separator className="bg-grey-1 my-4 mb-7" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Desription</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Enter description"
+                    rows={5}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
